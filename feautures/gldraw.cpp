a2f3f18d1f03727c78d002044e7d37724e1afdc9
thedraculaisreal <79720230+thedraculaisreal@users.noreply.github.com>
2024-10-20 18:26:57 +0000

    finished esp ahvent implemetned it yet tho

--- feautures/gldraw.cpp
+++ feautures/gldraw.cpp
@@ -16,14 +16,30 @@ void GL::setupOrtho()
 }
 void GL::restoreGl()
 {
-
+	glPopMatrix();
+	glPopAttrib();
 }
 
 void GL::drawFilledRect(float x, float y, float width, float height, const GLubyte color[3])
 {
-
+	glColor3ub(color[0], color[1], color[2]);
+	glBegin(GL_QUADS);
+	glVertex2f(x, y);
+	glVertex2f(x + width, y);
+	glVertex2f(x + width, y + height);
+	glVertex2f(x, y + height);
+	glEnd();
 }
+
 void GL::drawOutline(float x, float y, float width, float height, float lineWidth, const GLubyte color[3])
 {
-
+	glLineWidth(lineWidth);
+	glBegin(GL_LINE_STRIP);
+	glColor3ub(color[0], color[1], color[2]);
+	glVertex2f(x - 0.5f, y - 0.5f);
+	glVertex2f(x + width + 0.5f, y - 0.5f);
+	glVertex2f(x + width + 0.5f, y + height + 0.5f);
+	glVertex2f(x - 0.5f + width, y + height + 0.5f);
+	glVertex2f(x - 0.5f, y + height - 0.5f);
+	glEnd();
 }
